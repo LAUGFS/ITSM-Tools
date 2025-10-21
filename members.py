3ba3c94d9a87e79776a72d444d6c0061895df43a
Chavindu Nuwanpriya <chavindun@gmail.com>
2025-10-21 19:27:29 +0530

    Create members.py

---
+++ members.py
@@ -0,0 +1,11 @@
+employee_names = {
+  "191821": "Tharindu",
+  "191829": "Madhushan",
+  "191830": "Dineth",
+  "191933": "Ravijaya",
+  "191924": "Themiya",
+  "191922": "Kanishka",
+  "191941": "Ranuka",
+  "191942": "Milan",
+  "191970": "Rivishan",
+}
